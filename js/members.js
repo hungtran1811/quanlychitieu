0f81e64ba638abeedb236e17a316c8f96924c465
hungtran1811 <hungtran00.nt@gmail.com>
2025-09-08 22:41:23 +0700

    Update phase 2.2 file members

--- js/members.js
+++ js/members.js
@@ -1,9 +1,8 @@
-
 // js/members.js
 // ⚠️ Điền danh sách thành viên P102 của bạn tại đây (UID & tên).
 window.P102 = window.P102 || {};
 window.P102.MEMBERS = [
-  // { uid: "UID_HUNG", name: "Hưng" },
-  // { uid: "UID_B", name: "B" },
-  // { uid: "UID_C", name: "C" },
+  { uid: "rVrRomrCGbPqPcrPddtBTpdcLoh2", name: "Hưng" },
+  { uid: "Hl0rhLtXezawYU0Hwu23SwMWP413", name: "Thịnh" },
+  { uid: "vZJ0gOG9W8WYaN2mWbhWwOiU7xu2", name: "Thảo" },
 ];
