5a7feb30019bd071402fbfb11bae35dd64893776
hungtran1811 <hungtran00.nt@gmail.com>
2025-09-08 23:10:45 +0700

    Update phase 2.4

--- js/members.js
+++ js/members.js
@@ -1,6 +1,6 @@
 window.P102 = window.P102 || {};
 window.P102.MEMBERS = [
   { uid: "rVrRomrCGbPqPcrPddtBTpdcLoh2", name: "Hưng" },
-  { uid: "UID_B", name: "B" },
-  { uid: "UID_C", name: "C" },
+  { uid: "Hl0rhLtXezawYU0Hwu23SwMWP413", name: "Thịnh" },
+  { uid: "vZJ0gOG9W8WYaN2mWbhWwOiU7xu2", name: "Thảo" },
 ];
